a991569fed38a4e3992d7d7bd9bb4d0f06759709
joonsik <ryan0@kakao.com>
2021-12-08 11:32:34 +0900

    Intialize spring boot

---
+++ src/main/java/com/onfree/OnFreeApplication.java
@@ -0,0 +1,13 @@
+package com.onfree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnFreeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnFreeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/onfree/OnFreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onfree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnFreeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
