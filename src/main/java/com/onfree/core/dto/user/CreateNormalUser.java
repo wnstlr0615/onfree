1d4f025b49e1be216e030f3d4d86a5814ecdba33
joonsik <ryan0@kakao.com>
2021-12-11 22:58:47 +0900

    Implement CreateNormalUser,
    일반 유저 회원가입 기능 구현 및 컨트롤러 테스트 코드 작성

---
+++ src/main/java/com/onfree/core/dto/user/CreateNormalUser.java
@@ -0,0 +1,165 @@
+package com.onfree.core.dto.user;
+
+import com.onfree.core.entity.user.*;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.validation.constraints.*;
+
+@ApiModel(value = "CreateUserDto")
+public class CreateNormalUser {
+    @Getter
+    @Builder
+    public static class Request {
+        @ApiModelProperty(value = "사용자 이름", example = "김모씨")
+        @NotBlank(message = "이름은 공백일수 없습니다.")
+        private String name;
+
+        @ApiModelProperty(value = "이메일주소(사용자 아이디)", example = "jun@naver.com")
+        @Email(message = "이메일 형식이 올바르지 않습니다.")
+        @NotBlank(message = "이메일은 공백일 수 없습니다.")
+        private String email;
+
+        @ApiModelProperty(value = "사용자 비밀번호", example = "!abcdefghijk123")
+        @NotBlank(message = "비밀번호는 공백일 수 없습니다.")
+        private String password;
+
+        @ApiModelProperty(value = "통신사", example = "SKT", allowableValues = "KT,SKT,LG")
+        @NotBlank(message = "통신사는 공백일 수 없습니다.")
+        private String newsAgency; //통신사
+
+        @ApiModelProperty(value = "핸드폰번호", example = "010-0000-0000")
+        @NotBlank(message = "핸드폰번호는 공백일 수 없습니다.")
+        @Pattern(regexp = "^01(?:0|1|[6-9])-(?:\\d{3}|\\d{4})-\\d{4}$", message="핸드폰번호 패턴이 올바르지 않습니다.")
+        private String phoneNumber;
+
+        @ApiModelProperty(value = "은행명", example = "BUSAN_BANK" ,allowableValues = "${BankName.joinString()}" )
+        @NotNull(message = "은행명은 공백일 수 없습니다.")
+        private BankName bankName;
+
+        @ApiModelProperty(value = "계좌번호", example = "123456-456789-12")
+        @NotBlank(message = "계좌번호는 공백일 수 없습니다.")
+        private String accountNumber;
+
+        @AssertTrue(message = "서비스 동의를 하지 않으면 회원가입이 불가합니다.")
+        @ApiModelProperty(value = "서비스동의", example = "true")
+        @NotNull(message = "서비스 동의 체크는 필수 입니다.")
+        private Boolean serviceAgree;
+
+        @ApiModelProperty(value = "정책동의", example = "true")
+        @AssertTrue(message = "정책 동의를 하지 않으면 회원가입이 불가합니다.")
+        @NotNull(message = "정책 동의 체크는 필수 입니다.")
+        private Boolean policyAgree;
+
+        @ApiModelProperty(value = "개인정보동의", example = "true")
+        @AssertTrue(message = "개인정보 동의를 하지않으면 회원가입이 불가합니다.")
+        @NotNull(message = "개인정보 동의 체크는 필수 입니다.")
+        private Boolean personalInfoAgree;
+
+        @ApiModelProperty(value = "광고동의", example = "true")
+        @NotNull(message = "광고 동의 체크는 필수 입니다.")
+        private Boolean advertisementAgree;
+
+        @ApiModelProperty(value = "성인인증", example = "true")
+        @NotNull(message = "성인인증 체크는 필수 입니다.")
+        private Boolean adultCertification;
+
+        @ApiModelProperty(value = "MAN", example = "${Gender.joinString()}")
+        @NotNull(message = "성별입력은 필수 입니다.")
+        private Gender gender;
+
+        @ApiModelProperty(value = "프로필 URL", example = "http://onfree.io/images/546456498")
+        @NotBlank(message = "프로필 url은 필수입니다.")
+        private String profileImage;
+
+        public NormalUser toEntity() {
+            BankInfo bankInfo = BankInfo.builder()
+                    .bankName(bankName)
+                    .accountNumber(accountNumber)
+                    .build();
+            UserAgree userAgree = UserAgree.builder()
+                    .advertisement(advertisementAgree)
+                    .personalInfo(personalInfoAgree)
+                    .service(serviceAgree)
+                    .policy(policyAgree)
+                    .build();
+            return NormalUser.builder()
+                    .adultCertification(Boolean.TRUE)
+                    .email(email)
+                    .password(password)
+                    .gender(gender)
+                    .name(name)
+                    .newsAgency(newsAgency)
+                    .phoneNumber(phoneNumber)
+                    .bankInfo(bankInfo)
+                    .userAgree(userAgree)
+                    .adultCertification(adultCertification)
+                    .profileImage(profileImage)
+                    .role(Role.NORMAL)
+                    .build();
+        }
+    }
+
+    @Getter
+    @Builder
+    public static class Response{
+        @ApiModelProperty(value = "사용자 이름", example = "김모씨")
+        private String name;
+
+        @ApiModelProperty(value = "이메일주소(사용자 아이디)", example = "jun@naver.com")
+        private String email;
+
+        @ApiModelProperty(value = "통신사", example = "SKT", allowableValues = "KT,SKT,LG")
+        private String newsAgency; //통신사
+
+
+        @ApiModelProperty(value = "핸드폰번호", example = "010-0000-0000")
+        private String phoneNumber;
+
+        @ApiModelProperty(value = "은행명", example = "BUSAN_BANK" ,allowableValues = "${BankName.joinString()}" )
+        private String bankName;
+
+        @ApiModelProperty(value = "계좌번호", example = "123456-456789-12")
+        private String accountNumber;
+
+        @ApiModelProperty(value = "서비스동의", example = "true")
+        private Boolean serviceAgree;
+
+        @ApiModelProperty(value = "정책동의", example = "true")
+        private Boolean policyAgree;
+
+        @ApiModelProperty(value = "개인정보동의", example = "true")
+        private Boolean personalInfoAgree;
+
+        @ApiModelProperty(value = "광고동의", example = "true")
+        private Boolean advertisementAgree;
+
+        @ApiModelProperty(value = "성인인증", example = "true")
+        private Boolean adultCertification;
+
+        @ApiModelProperty(value = "성별", example = "${Gender.joinString()}")
+        private String gender;
+
+        @ApiModelProperty(value = "프로필 URL", example = "http://onfree.io/images/546456498")
+        private String profileImage;
+
+        public static Response fromEntity(NormalUser entity) {
+            return Response.builder()
+                    .adultCertification(entity.getAdultCertification())
+                    .email(entity.getEmail())
+                    .gender(entity.getGender().getName())
+                    .name(entity.getName())
+                    .phoneNumber(entity.getPhoneNumber())
+                    .bankName(entity.getBankInfo().getBankName().getBankName())
+                    .accountNumber(entity.getBankInfo().getAccountNumber())
+                    .advertisementAgree(entity.getUserAgree().getAdvertisement())
+                    .personalInfoAgree(entity.getUserAgree().getPersonalInfo())
+                    .policyAgree(entity.getUserAgree().getPolicy())
+                    .serviceAgree(entity.getUserAgree().getService())
+                    .profileImage(entity.getProfileImage())
+                    .build();
+        }
+    }
+}
---
+++ src/test/java/com/onfree/controller/NormalUserControllerTest.java
@@ -0,0 +1,181 @@
+package com.onfree.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.onfree.core.dto.user.CreateNormalUser;
+import com.onfree.core.entity.user.BankName;
+import com.onfree.core.entity.user.Gender;
+import com.onfree.core.service.UserService;
+import com.onfree.error.code.ErrorCode;
+import com.onfree.error.code.UserErrorCode;
+import com.onfree.error.exception.UserException;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = NormalUserController.class)
+@AutoConfigureMockMvc
+class NormalUserControllerTest {
+    @Autowired
+    private MockMvc mvc;
+    @Autowired
+    private ObjectMapper mapper;
+
+    @MockBean
+    private UserService userService;
+
+    @Test
+    @DisplayName("[성공][POST] 회원가입 요청")
+    public void givenCreateUserReq_whenCreateNormalUser_thenCreateUserRes() throws Exception{
+        //given
+        CreateNormalUser.Request request = givenCreateNormalUserReq();
+        CreateNormalUser.Response response = givenCreateNormalUserRes();
+        when(userService.createNormalUser(any()))
+                .thenReturn(response);
+
+        //when //then
+        mvc.perform(post("/api/users/normal")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(
+                        mapper.writeValueAsString(
+                                request
+                        )
+                )
+        )
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.name").value("준식"))
+            .andExpect(jsonPath("$.email").value("jun@naver.com"))
+            .andExpect(jsonPath("$.newsAgency").value("SKT"))
+            .andExpect(jsonPath("$.phoneNumber").value("010-8888-9999"))
+            .andExpect(jsonPath("$.bankName").value(BankName.IBK_BANK.getBankName()))
+            .andExpect(jsonPath("$.accountNumber").value("010-8888-9999"))
+            .andExpect(jsonPath("$.serviceAgree").value(true))
+            .andExpect(jsonPath("$.policyAgree").value(true))
+            .andExpect(jsonPath("$.personalInfoAgree").value(true))
+            .andExpect(jsonPath("$.advertisementAgree").value(true))
+            .andExpect(jsonPath("$.adultCertification").value(true))
+            .andExpect(jsonPath("$.gender").value(Gender.MAN.getName()))
+            .andExpect(jsonPath("$.profileImage").value("http://onfree.io/images/123456789"))
+        ;
+        verify(userService, times(1)).createNormalUser(any());
+    }
+    private CreateNormalUser.Request givenCreateNormalUserReq() {
+        return CreateNormalUser.Request
+                .builder()
+                .adultCertification(Boolean.TRUE)
+                .email("jun@naver.com")
+                .password("!Abcderghijk112")
+                .gender(Gender.MAN)
+                .name("준식")
+                .newsAgency("SKT")
+                .phoneNumber("010-8888-9999")
+                .bankName(BankName.IBK_BANK)
+                .accountNumber("010-8888-9999")
+                .advertisementAgree(true)
+                .personalInfoAgree(true)
+                .policyAgree(true)
+                .serviceAgree(true)
+                .profileImage("http://onfree.io/images/123456789")
+                .build();
+    }
+    private CreateNormalUser.Response givenCreateNormalUserRes(){
+        return CreateNormalUser.Response
+                .builder()
+                .adultCertification(Boolean.TRUE)
+                .email("jun@naver.com")
+                .gender(Gender.MAN.getName())
+                .name("준식")
+                .newsAgency("SKT")
+                .phoneNumber("010-8888-9999")
+                .bankName(BankName.IBK_BANK.getBankName())
+                .accountNumber("010-8888-9999")
+                .advertisementAgree(true)
+                .personalInfoAgree(true)
+                .policyAgree(true)
+                .serviceAgree(true)
+                .profileImage("http://onfree.io/images/123456789")
+                .build();
+    }
+    @Test
+    @DisplayName("[실패][POST] 회원가입 요청 - 회원가입 request가 올바르지 않은 경우")
+    public void givenWrongCreateUserReq_whenCreateNormalUser_thenParameterValidError() throws Exception{
+        //given
+        CreateNormalUser.Request request = givenWrongCreateNormalUserReq();
+        ErrorCode errorCode=UserErrorCode.NOT_VALID_REQUEST_PARAMETERS;
+        //when //then
+        mvc.perform(post("/api/users/normal")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(
+                        mapper.writeValueAsString(
+                                request
+                        )
+                )
+        )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("errorCode").value(errorCode.toString()))
+                .andExpect(jsonPath("errorMessage").value(errorCode.getDescription()))
+        ;
+        verify(userService, never()).createNormalUser(any());
+    }
+
+    private CreateNormalUser.Request givenWrongCreateNormalUserReq() {
+        return CreateNormalUser.Request
+                .builder()
+                .adultCertification(Boolean.TRUE)
+                .email("")
+                .password("!Abcderghijk112")
+                .gender(Gender.MAN)
+                .name("")
+                .newsAgency("SKT")
+                .phoneNumber("010-8888-9999")
+                .bankName(null)
+                .accountNumber("010-8888-9999")
+                .advertisementAgree(true)
+                .personalInfoAgree(true)
+                .policyAgree(true)
+                .serviceAgree(false)
+                .profileImage("http://onfree.io/images/123456789")
+                .build();
+    }
+
+    @Test
+    @DisplayName("[실패][POST] 회원가입 요청 - 이메일 중복으로 인한 회원가입 실패")
+    public void givenDuplicatedEmail_whenCreateNormalUser_thenDuplicatedEmailError() throws Exception{
+        //given
+        CreateNormalUser.Request request = givenCreateNormalUserReq();
+        ErrorCode errorCode = UserErrorCode.USER_EMAIL_DUPLICATED;
+        when(userService.createNormalUser(any()))
+                .thenThrow( new UserException(errorCode));
+        //when //then
+        mvc.perform(post("/api/users/normal")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(
+                        mapper.writeValueAsString(
+                                request
+                        )
+                )
+        )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("errorCode").value(errorCode.toString()))
+                .andExpect(jsonPath("errorMessage").value(errorCode.getDescription()))
+        ;
+        verify(userService, times(1)).createNormalUser(any());
+
+    }
+
+}
