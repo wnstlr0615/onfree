e41123da76ce91891dcee82bd5d6d0605b1ed5b9
joonsik <ryan0@kakao.com>
2021-12-18 22:04:41 +0900

    Add ArtistUser Dto 및 Controller , Service 추가
    작가 회원가입 , 조회, 수정, 삭제 기능 추가

---
+++ src/main/java/com/onfree/core/dto/user/artist/UpdateArtistUser.java
@@ -0,0 +1,104 @@
+package com.onfree.core.dto.user.artist;
+
+import com.onfree.core.entity.user.ArtistUser;
+import com.onfree.core.entity.user.BankName;
+import com.onfree.core.entity.user.NormalUser;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+public class UpdateArtistUser {
+    @ApiModel(value = "UpdateArtistUser_Request")
+    @Builder
+    @Getter
+    public static class Request{
+        @ApiModelProperty(value = "사용자 닉네임", example = "온프리대박기원")
+        @NotBlank(message = "이름은 공백일수 없습니다.")
+        private final String nickname;
+
+        @ApiModelProperty(value = "통신사", example = "SKT", allowableValues = "KT,SKT,LG")
+        @NotBlank(message = "통신사는 공백일 수 없습니다.")
+        private final String newsAgency; //통신사
+
+        @ApiModelProperty(value = "핸드폰번호", example = "010-0000-0000")
+        @NotBlank(message = "핸드폰번호는 공백일 수 없습니다.")
+        @Pattern(regexp = "^01(?:0|1|[6-9])-(?:\\d{3}|\\d{4})-\\d{4}$", message="핸드폰번호 패턴이 올바르지 않습니다.")
+        private final String phoneNumber;
+
+        @ApiModelProperty(value = "은행명", example = "BUSAN_BANK" ,allowableValues = " ${BankName.joinString()}" )
+        @NotNull(message = "은행명은 공백일 수 없습니다.")
+        private final BankName bankName;
+
+        @ApiModelProperty(value = "계좌번호", example = "123456-456789-12")
+        @NotBlank(message = "계좌번호는 공백일 수 없습니다.")
+        private final String accountNumber;
+
+        @ApiModelProperty(value = "성인인증", example = "true")
+        @NotNull(message = "성인인증 체크는 필수 입니다.")
+        private final Boolean adultCertification;
+
+        @ApiModelProperty(value = "프로필 URL", example = "http://onfree.io/images/546456498")
+        @NotBlank(message = "프로필 url은 필수입니다.")
+        private final String profileImage;
+
+        @ApiModelProperty(value = "포트폴리오 개인룸 URL", example = "http://onfree.io/portfoliourl/546456498")
+        @NotBlank(message = "포트폴리오 개인룸 URL 은 필수입니다.")
+        private final String portfolioUrl;
+
+    }
+    @Getter
+    @Builder
+    @ApiModel(value = "UpdateArtistUser_Response")
+    public static class Response{
+        @ApiModelProperty(value = "사용자 닉네임", example = "온프리대박기원")
+        @NotBlank(message = "이름은 공백일수 없습니다.")
+        private final String nickname;
+
+        @ApiModelProperty(value = "통신사", example = "SKT", allowableValues = "KT,SKT,LG")
+        @NotBlank(message = "통신사는 공백일 수 없습니다.")
+        private final String newsAgency; //통신사
+
+        @ApiModelProperty(value = "핸드폰번호", example = "010-0000-0000")
+        @NotBlank(message = "핸드폰번호는 공백일 수 없습니다.")
+        @Pattern(regexp = "^01(?:0|1|[6-9])-(?:\\d{3}|\\d{4})-\\d{4}$", message="핸드폰번호 패턴이 올바르지 않습니다.")
+        private final String phoneNumber;
+
+        @ApiModelProperty(value = "은행명", example = "BUSAN_BANK" ,allowableValues = " ${BankName.joinString()}" )
+        @NotNull(message = "은행명은 공백일 수 없습니다.")
+        private final BankName bankName;
+
+        @ApiModelProperty(value = "계좌번호", example = "123456-456789-12")
+        @NotBlank(message = "계좌번호는 공백일 수 없습니다.")
+        private final String accountNumber;
+
+        @ApiModelProperty(value = "성인인증", example = "true")
+        @NotNull(message = "성인인증 체크는 필수 입니다.")
+        private final Boolean adultCertification;
+
+        @ApiModelProperty(value = "프로필 URL", example = "http://onfree.io/images/546456498")
+        @NotBlank(message = "프로필 url은 필수입니다.")
+        private final String profileImage;
+
+        @ApiModelProperty(value = "포트폴리오 개인룸 URL", example = "http://onfree.io/portfoliourl/546456498")
+        @NotBlank(message = "포트폴리오 개인룸 URL 은 필수입니다.")
+        private final String portfolioUrl;
+
+        public static UpdateArtistUser.Response fromEntity(ArtistUser entity){
+            return UpdateArtistUser.Response.builder()
+                    .nickname(entity.getNickname())
+                    .bankName(entity.getBankInfo().getBankName())
+                    .accountNumber(entity.getBankInfo().getAccountNumber())
+                    .newsAgency(entity.getNewsAgency())
+                    .phoneNumber(entity.getPhoneNumber())
+                    .profileImage(entity.getProfileImage())
+                    .adultCertification(entity.getAdultCertification())
+                    .portfolioUrl(entity.getPortfolioUrl())
+                    .build();
+        }
+    }
+}
---
+++ src/main/java/com/onfree/core/service/ArtistUserService.java
@@ -0,0 +1,104 @@
+package com.onfree.core.service;
+
+import com.onfree.core.dto.user.DeletedUserResponse;
+import com.onfree.core.dto.user.artist.ArtistUserDetail;
+import com.onfree.core.dto.user.artist.CreateArtistUser;
+import com.onfree.core.dto.user.artist.UpdateArtistUser;
+import com.onfree.core.entity.user.ArtistUser;
+import com.onfree.core.repository.UserRepository;
+import com.onfree.error.exception.UserException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import static com.onfree.error.code.UserErrorCode.*;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ArtistUserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    /** 회원가입 요청 */
+    @Transactional
+    public CreateArtistUser.Response createArtistUser(CreateArtistUser.Request request) {
+        duplicatedUserEmail(request.getEmail());
+        return CreateArtistUser.Response
+                .fromEntity(
+                    saveArtistUser(
+                            bcryptPassword(request.toEntity())
+                    )
+        );
+    }
+
+    private ArtistUser bcryptPassword(ArtistUser artistUser) {
+        artistUser.encryptPassword(
+                passwordEncoder.encode(
+                        artistUser.getPassword()
+                )
+        );
+        return artistUser;
+    }
+
+    private ArtistUser saveArtistUser(ArtistUser ArtistUser) {
+        return userRepository.save(ArtistUser);
+    }
+
+    private void duplicatedUserEmail(String email) {
+        Integer cnt = getCountByEmail(email);
+        if(cnt > 0){
+            throw new UserException(USER_EMAIL_DUPLICATED);
+        }
+    }
+
+    private Integer getCountByEmail(String email) {
+        return userRepository.countByEmail(
+                email
+        );
+    }
+    /** 사용자 정보 조회*/
+    public ArtistUserDetail getUserDetail(Long userId) {
+        return ArtistUserDetail.fromEntity(
+                getArtistUser(userId)
+        );
+    }
+
+    private ArtistUser getArtistUser(Long userId) {
+        return (ArtistUser) userRepository.findById(userId)
+                .orElseThrow(
+                        () -> new UserException(NOT_FOUND_USERID)
+                );
+    }
+    /** 사용자 deleted 처리*/
+    @Transactional
+    public DeletedUserResponse deletedArtistUser(Long userId) {
+        return DeletedUserResponse.fromEntity(
+                setArtistUserDeleted(
+                        getArtistUser(userId)
+                )
+        );
+    }
+
+    private ArtistUser setArtistUserDeleted(ArtistUser ArtistUser) {
+        if(ArtistUserIsDeleted(ArtistUser)){
+            throw new UserException(ALREADY_USER_DELETED);
+        }
+        ArtistUser.setDeleted();
+        return ArtistUser;
+    }
+
+    private boolean ArtistUserIsDeleted(ArtistUser ArtistUser) {
+        return ArtistUser.getDeleted();
+    }
+
+    /** 사용자 계정 수정*/
+    @Transactional
+    public UpdateArtistUser.Response modifiedUser(Long userId, UpdateArtistUser.Request request) {
+        ArtistUser ArtistUser = getArtistUser(userId);
+        ArtistUser.update(request);
+        return UpdateArtistUser.Response
+                .fromEntity(ArtistUser);
+    }
+}
