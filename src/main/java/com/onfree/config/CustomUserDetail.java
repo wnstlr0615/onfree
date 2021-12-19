adbdf21aeec2479c00d3ee2ef777362021c19759
joonsik <ryan0@kakao.com>
2021-12-19 13:34:57 +0900

    Modify SecurityConfig , Add CustomUser
    시큐리티 접근 url 수정 및 CustomUserDetail, CustomUserDetailService 구현

---
+++ src/main/java/com/onfree/config/CustomUserDetail.java
@@ -0,0 +1,26 @@
+package com.onfree.config;
+
+import com.onfree.core.entity.user.Role;
+import com.onfree.core.entity.user.User;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collection;
+import java.util.Set;
+
+public class CustomUserDetail extends org.springframework.security.core.userdetails.User {
+    User user;
+
+    public User getUser() {
+        return user;
+    }
+
+    public CustomUserDetail(User user) {
+        super(user.getEmail(), user.getPassword(), getAuthority(user.getRole()));
+    }
+
+    private static Collection<? extends GrantedAuthority> getAuthority(Role role) {
+        return Set.of(new SimpleGrantedAuthority(role.name()));
+    }
+}
--- src/main/java/com/onfree/config/SecurityConfig.java
+++ src/main/java/com/onfree/config/SecurityConfig.java
@@ -1,8 +1,11 @@
 package com.onfree.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -14,21 +17,37 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final CustomUserDetailService userDetailService;
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 
     @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailService)
+                .passwordEncoder(passwordEncoder());
+
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/**").permitAll();
+                //.antMatchers("/**").permitAll();
+                .antMatchers(HttpMethod.POST,"/api/users/artist", "/api/users/normal").anonymous()
+                .antMatchers("/api/users/artist/**").hasRole("ARTIST")
+                .antMatchers("/api/users/normal/**").hasRole("NORMAL")
+                .anyRequest().authenticated();
 
         http.httpBasic().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.formLogin().disable();
+        http.formLogin()
+                .loginProcessingUrl("/login")
+                .permitAll();
+
         http.csrf().disable();
     }
 
