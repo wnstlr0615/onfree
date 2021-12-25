ed4206f51da1a6f03e3f1b6eae51dd980ef85158
joonsik <ryan0@kakao.com>
2021-12-25 21:38:37 +0900

    Modify Controller Bean Valid 에러 필드에러 추가 Bean Valid 에러시 기존에는 에러코드만 전송 했지만 FieldError를 FieldDto.class 로 변환 하여 추가

--- src/main/java/com/onfree/error/exception/response/ResponseResult.java
+++ src/main/java/com/onfree/error/exception/response/ResponseResult.java
@@ -1,8 +1,11 @@
 package com.onfree.error.exception.response;
 
 import com.onfree.error.code.ErrorCode;
+import com.onfree.error.exception.FieldErrorDto;
 import lombok.Getter;
 import org.springframework.http.ResponseEntity;
+
+import java.util.List;
 
 @Getter
 public class ResponseResult {
@@ -11,6 +14,13 @@ public class ResponseResult {
                 code.getStatus()
         ).body(
                 SimpleErrorResponse.fail(code)
+        );
+    }
+    public static ResponseEntity<?> fail(ErrorCode code, List<FieldErrorDto> fieldErrors){
+        return ResponseEntity.status(
+                code.getStatus()
+        ).body(
+                SimpleErrorResponse.fail(code, fieldErrors)
         );
     }
 
--- src/main/java/com/onfree/error/exception/response/SimpleErrorResponse.java
+++ src/main/java/com/onfree/error/exception/response/SimpleErrorResponse.java
@@ -1,10 +1,14 @@
 package com.onfree.error.exception.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.onfree.error.code.ErrorCode;
+import com.onfree.error.exception.FieldErrorDto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Getter
 @Builder
@@ -12,12 +16,24 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class SimpleErrorResponse {
     private ErrorCode errorCode;
+
     private String errorMessage;
+
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    private List<FieldErrorDto> errors;
 
     public static SimpleErrorResponse fail(ErrorCode errorCode){
        return  SimpleErrorResponse.builder()
                .errorCode(errorCode)
                .errorMessage(errorCode.getDescription())
                .build();
+    }
+
+    public static SimpleErrorResponse fail(ErrorCode errorCode, List<FieldErrorDto> fieldErrors) {
+        return SimpleErrorResponse.builder()
+                .errorCode(errorCode)
+                .errorMessage(errorCode.getDescription())
+                .errors(fieldErrors)
+                .build();
     }
 }
