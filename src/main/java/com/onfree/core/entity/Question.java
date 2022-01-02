994905a307dd4c1536abbc4d24ceb5798079f1ba
joonsik <ryan0@kakao.com>
2022-01-02 21:25:05 +0900

    ADD NoticeEntity, QuestionEntity 추가 공지 entity와 question entity 추가

---
+++ src/main/java/com/onfree/core/entity/Question.java
@@ -0,0 +1,38 @@
+package com.onfree.core.entity;
+
+import com.onfree.core.model.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+public class Question extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long questionId;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column(nullable = false, length = 2000)
+    private String content;
+
+    @Column(nullable = false)
+    private boolean top;
+
+    @Column(nullable = false)
+    private boolean disabled;
+
+    @Column(nullable = false)
+    private int view;
+
+    public void updateView() {
+        view = view + 1;
+    }
+}
