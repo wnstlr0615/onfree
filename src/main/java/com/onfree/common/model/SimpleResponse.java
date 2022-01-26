7046b54d72d40637280f4f4f1697ed918a0b4b61
wnstlr0615 <wnstlr0615@naver.com>
2022-01-26 19:08:23 +0900

    Add SimpleResponse data 필드 추가 SimpleResponse 클래스에  data 필드 추가

--- src/main/java/com/onfree/common/model/SimpleResponse.java
+++ src/main/java/com/onfree/common/model/SimpleResponse.java
@@ -1,18 +1,28 @@
 package com.onfree.common.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class SimpleResponse {
     private boolean result;
     private String message;
+    private Object data;
 
     public static SimpleResponse success(String message){
         return SimpleResponse.builder()
                 .result(true)
                 .message(message)
+                .build();
+    }
+    public static SimpleResponse success(String message, Object data){
+        return SimpleResponse.builder()
+                .result(true)
+                .message(message)
+                .data(data)
                 .build();
     }
 
