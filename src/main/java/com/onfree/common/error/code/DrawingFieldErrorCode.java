1c0d27ce0f7a653cb3964c3d032e3e96b313b0e6
wnstlr0615 <wnstlr0615@naver.com>
2022-01-26 19:15:49 +0900

    Add DrawingFieldRepository 쿼리 메소드 추가 및 에러코드 추가

--- src/main/java/com/onfree/common/error/code/DrawingFieldErrorCode.java
+++ src/main/java/com/onfree/common/error/code/DrawingFieldErrorCode.java
@@ -8,7 +8,9 @@ import lombok.Getter;
 public enum DrawingFieldErrorCode implements ErrorCode{
     NOT_FOUND_DRAWING_FIELD("해당 그림분야를 찾을 수 없습니다.",404),
     DUPLICATED_DRAWING_FIELD_NAME("해당 그림 분야명은 이미 등록되어 있습니다.",400),
-    DRAWING_FIELD_EMPTY("등록된 그림 분야 명이 없습니다.", 400)
+    DRAWING_FIELD_EMPTY("등록된 그림 분야 명이 없습니다.", 400),
+
+    WRONG_DRAWING_FIELD("잘못된 그림분야를 입력하였습니다.", 400)
     ;
     private final String description;
     private final int status;
--- src/main/java/com/onfree/core/repository/DrawingFieldRepository.java
+++ src/main/java/com/onfree/core/repository/DrawingFieldRepository.java
@@ -3,12 +3,19 @@ package com.onfree.core.repository;
 import com.onfree.core.entity.DrawingField;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
 public interface DrawingFieldRepository extends JpaRepository<DrawingField, Long> {
+    /** Top이 True인 그림분야를 우선으로 하여 모든 그림분야 가져오기 */
     List<DrawingField> findAllByOrderByTopDesc();
 
+    /** 삭제 처리 되지 않은 데이터중 Top이 true인 그림분야를 우선으로 하는 모든 그림 분야 가져오기*/
+    List<DrawingField> findAllByDisabledIsFalseOrderByTopDesc();
+
+    /** 필드이름(unique)으로 그림분야 가져오기 */
     Optional<DrawingField> findByFieldName(String fieldName);
+
+    /** 삭제 처리 되지 않은 그림 중 해당 그림 분야 ID와 일치하는 그림분야 가져오기 */
+    List<DrawingField> findAllByDisabledIsFalseAndDrawingFieldIdIn(List<Long> drawingFieldId);
 }
