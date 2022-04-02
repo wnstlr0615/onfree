e3542acd92b06bdbd44ac983302353c39a15e6f4
wnstlr0615 <wnstlr0615@naver.com>
2022-04-02 13:31:15 +0900

    Add JsonUtils, RestTemplateUtils 추가 코드 중복을 방지하기 위해 유틸추가

---
+++ src/main/java/com/onfree/utils/JsonUtils.java
@@ -0,0 +1,32 @@
+package com.onfree.utils;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class JsonUtils {
+    private final ObjectMapper mapper;
+
+    public String toJson(Object o) throws JsonProcessingException {
+        try {
+            return mapper.writeValueAsString(o);
+        } catch (JsonProcessingException e) {
+            log.error("fail JsonUtils toJson Error - transfer type : {}", o.getClass().getName());
+            throw e;
+        }
+    }
+
+    public <T> T fromJson(String json, Class<T> responseType) throws JsonProcessingException {
+        try {
+            return mapper.readValue(json, responseType);
+        } catch (JsonProcessingException e) {
+            log.error("fail JsonUtils fromJson Error - transfer type : {}", responseType.getName());
+            throw e;
+        }
+    }
+}
